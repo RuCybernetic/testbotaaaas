29b1944435a09f2d9f011586a1d1df4039fb9104
RuCybernetic <47589158+RuCybernetic@users.noreply.github.com>
2019-12-11 21:17:04 +0300

    Update Cyber_bot.py

--- Cyber_bot.py
+++ Cyber_bot.py
@@ -40,6 +40,39 @@ async def on_member_join(member):
         await member.add_roles(role)
     else:
         pass
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.message_id != 654382178003583007:
+        return
+    if str(payload.emoji) == "<:LoL:654146562468872214>":
+        role = client.get_guild(payload.guild_id).get_role(653834600602402827)
+        member = client.get_guild(payload.guild_id).get_member(payload.user_id)
+        await member.add_roles(role)
+    elif str(payload.emoji) == "<:Hots:654150946456338463>":
+        role = client.get_guild(payload.guild_id).get_role(653835527497449472)
+        member = client.get_guild(payload.guild_id).get_member(payload.user_id)
+        await member.add_roles(role)
+    elif str(payload.emoji) == "<:diablo3_2:654159805694476298>":
+        role = client.get_guild(payload.guild_id).get_role(653835076853039104)
+        member = client.get_guild(payload.guild_id).get_member(payload.user_id)
+        await member.add_roles(role)
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    if payload.message_id != 654382178003583007:
+        return
+    if str(payload.emoji) == "<:LoL:654146562468872214>":
+        role = client.get_guild(payload.guild_id).get_role(653834600602402827)
+        member = client.get_guild(payload.guild_id).get_member(payload.user_id)
+        await member.remove_roles(role)
+    elif str(payload.emoji) == "<:Hots:654150946456338463>":
+        role = client.get_guild(payload.guild_id).get_role(653835527497449472)
+        member = client.get_guild(payload.guild_id).get_member(payload.user_id)
+        await member.remove_roles(role)
+    elif str(payload.emoji) == "<:diablo3_2:654159805694476298>":
+        role = client.get_guild(payload.guild_id).get_role(653835076853039104)
+        member = client.get_guild(payload.guild_id).get_member(payload.user_id)
+        await member.remove_roles(role)
 
 @client.command(aliases=['привет'])
 async def hello(ctx):
